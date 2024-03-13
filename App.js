5764204077c3dae21628de7cbc7df1853de110b7
Rahul Mujumdar <rahulmujumdar2000@gmail.com>
2024-03-13 15:07:01 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
+import tw from "tailwind-react-native-classnames";
 
 export default function App() {
+  const increment = () => {
+    setNumber((prev) => prev + 1);
+  };
+
+  const printName = () => {
+    alert(name);
+    setName("");
+  };
+
+  const [number, setNumber] = useState(0);
+  const [name, setName] = useState("");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={tw`flex items-center m-auto`}>
+      <Text style={tw`font-bold text-xl`}>Hello World!</Text>
+      <Text style={tw`font-bold text-xl`}>{number}</Text>
+      <TouchableOpacity
+        onPress={increment}
+        style={tw`p-2 border rounded-md my-2`}
+      >
+        <Text>+</Text>
+      </TouchableOpacity>
+      <TextInput
+        style={tw`border px-10`}
+        value={name}
+        onChangeText={(e) => setName(e)}
+        onSubmitEditing={printName}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
